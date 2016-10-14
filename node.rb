5caea9cd9e1292ed04d06bb55c130a08bda520de
Neurodynamic <neurodynamicdev@gmail.com>
2016-10-14 15:31:55 -0400

    remove implementations

--- node.rb
+++ node.rb
@@ -1,21 +1,2 @@
 class Node
-  attr_accessor :element
-  attr_reader :next
-
-  def initialize(element)
-    @element = element
-    @next = nil
-  end
-
-  def insert_after(node)
-    self.next = node
-  end
-
-  def remove_after
-    self.next = nil
-  end
-
-  private
-
-  attr_writer :next
 end
