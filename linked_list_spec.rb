7a085acbb0ed77a2acaac4d7f47a2895a6656b62
Neurodynamic <neurodynamicdev@gmail.com>
2016-10-14 15:11:24 -0400

    add linked list spec and implementation

--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -1,78 +1,92 @@
 require_relative 'linked_list'
-# require_relative 'array_list'
 
-# describe LinkedList do
-#   let(:array_list) { LinkedList.new }
+describe LinkedList do
+  let(:linked_list) { LinkedList.new }
 
-#   before do
-#     array_list.add(:thing_1)
-#     array_list.add(:thing_2)
-#     array_list.add(:thing_3)
-#   end
+  before do
+    linked_list.insert_last(:thing_1)
+    linked_list.insert_last(:thing_2)
+    linked_list.insert_last(:thing_3)
+  end
 
-#   describe '#add' do
-#     it 'adds an element to the end of the list' do
-#       array_list.add(:thing_4)
-#       expect(array_list.get(3)).to eq(:thing_4)
-#     end
+  describe '#insert_first' do
+    it 'adds an element to the front of the list' do
+      linked_list.insert_first(:new_thing)
+      expect(linked_list.get(0)).to eq(:new_thing)
+      expect(linked_list.size).to eq(4)
+    end
+  end
+
+  describe '#remove_first' do
+    it 'removes an element from the front of the list' do
+      linked_list.remove_first
+      expect(linked_list.get(0)).to eq(:thing_2)
+      expect(linked_list.size).to eq(2)
+    end
+  end
+
+  describe '#insert_last' do
+    it 'adds an element to the front of the list' do
+      linked_list.insert_last(:new_thing)
+      expect(linked_list.get(3)).to eq(:new_thing)
+    end
+  end
+
+  describe '#remove_last' do
+    it 'removes an element from the end of the list' do
+      linked_list.remove_last
+
+      expect(linked_list.get(0)).to eq(:thing_1)
+      expect(linked_list.get(1)).to eq(:thing_2)
+      expect(linked_list.size).to eq(2)
+    end
+  end
+
+  describe '#get' do
+    it 'returns element at specified index' do
+      expect(linked_list.get(0)).to eq(:thing_1)
+      expect(linked_list.get(1)).to eq(:thing_2)
+      expect(linked_list.get(2)).to eq(:thing_3)
+    end
+
+    it 'throws error if no element exists at specified index' do
+      expect { linked_list.get(3) }
+        .to raise_error(LinkedList::OutOfBoundsError)
+    end
+  end
+
+  describe '#set' do
+    it 'replaces element at the specified index' do
+      linked_list.set(1, :new_thing)
+      expect(linked_list.get(1)).to eq(:new_thing)
+    end
     
-#     it 'returns the element added' do
-#       expect(array_list.add(:thing_4)).to eq(:thing_4)
-#     end
+    it 'throws error if no element exists at specified index' do
+      expect { linked_list.set(3, :new_thing) }
+        .to raise_error(LinkedList::OutOfBoundsError)
+    end
+  end
+
+  describe '#insert' do
+    it 'adds element with specified value at the specified index' do
+      linked_list.insert(1, :new_thing)
+      expect(linked_list.get(0)).to eq(:thing_1)
+      expect(linked_list.get(1)).to eq(:new_thing)
+      expect(linked_list.get(2)).to eq(:thing_2)
+      expect(linked_list.get(3)).to eq(:thing_3)
+    end
     
-#     it 'handles lots of adds' do
-#       4.upto(15).each do |i|
-#         array_list.add("thing_#{i}".to_sym)
-#       end
-#       expect(array_list.length).to eq(15)
-#     end
-#   end
+    it 'throws error if no element exists at specified index' do
+      expect { linked_list.insert(3, :new_thing) }
+        .to raise_error(LinkedList::OutOfBoundsError)
+    end
+  end
 
-#   describe '#get' do
-#     it 'returns element at specified index' do
-#       expect(array_list.get(0)).to eq(:thing_1)
-#       expect(array_list.get(1)).to eq(:thing_2)
-#       expect(array_list.get(2)).to eq(:thing_3)
-#     end
-
-#     it 'throws error if no element exists at specified index' do
-#       expect { array_list.get(3) }
-#         .to raise_error(LinkedList::OutOfBoundsError)
-#     end
-#   end
-
-#   describe '#set' do
-#     it 'replaces element at the specified index' do
-#       array_list.set(1, :new_thing)
-#       expect(array_list.get(1)).to eq(:new_thing)
-#     end
-    
-#     it 'throws error if no element exists at specified index' do
-#       expect { array_list.set(3, :new_thing) }
-#         .to raise_error(LinkedList::OutOfBoundsError)
-#     end
-#   end
-
-#   describe '#insert' do
-#     it 'adds element with specified value at the specified index' do
-#       array_list.insert(1, :new_thing)
-#       expect(array_list.get(0)).to eq(:thing_1)
-#       expect(array_list.get(1)).to eq(:new_thing)
-#       expect(array_list.get(2)).to eq(:thing_2)
-#       expect(array_list.get(3)).to eq(:thing_3)
-#     end
-    
-#     it 'throws error if no element exists at specified index' do
-#       expect { array_list.insert(3, :new_thing) }
-#         .to raise_error(LinkedList::OutOfBoundsError)
-#     end
-#   end
-
-#   describe '#length' do
-#     it 'returns number of items in list' do
-#       expect(array_list.length).to eq(3)
-#       array_list.add(:thing_4)
-#       expect(array_list.length).to eq(4)
-#     end
-#   end
-# end
+  describe '#size' do
+    it 'returns number of items in list' do
+      expect(linked_list.size).to eq(3)
+      linked_list.insert_last(:thing_4)
+      expect(linked_list.size).to eq(4)
+    end
+  end
+end
