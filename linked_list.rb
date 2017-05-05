55b57172d9e6f51c98e366f2330a7c246dfe1d72
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-05-04 20:02:36 -0700

    Write tests for LinkedList#insert_first

--- linked_list.rb
+++ linked_list.rb
@@ -2,4 +2,36 @@ require_relative 'node'
 
 class LinkedList
 
+  attr_reader :size
+  attr_accessor :first
+
+  def initialize
+    @first = nil
+    @size = 0
+  end
+
+  def length
+    @size
+  end
+
+  def insert_first(element)
+    if @first
+      new_first = Node.new(element)
+      new_first.insert_after(@first)
+      @first = new_first
+    else
+      @first = Node.new(element)
+    end
+    @size += 1
+  end
+
+  def remove_first
+  end
+
+  def insert_last(element)
+  end
+
+  def remove_last
+  end
+
 end
--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -2,4 +2,43 @@ require_relative 'linked_list'
 
 describe LinkedList do
 
+  describe "let" do
+
+    let(:linked_list) { LinkedList.new }
+
+    it "creates a new empty linked list" do
+      expect(linked_list.first).to be nil
+    end
+
+    it "sets first to a node" do
+      linked_list.insert_first("node 1")
+      expect(linked_list.first).to be_a(Node)
+    end
+
+    it "inserts a new node in front" do
+      linked_list.insert_first("node 1")
+      linked_list.insert_first("node 2")
+      expect(linked_list.first.value).to eq("node 2")
+    end
+
+    it "increases the length after inserting" do
+      linked_list.insert_first("node 1")
+      linked_list.insert_first("node 2")
+      expect(linked_list.length).to eq 2
+    end
+
+    it "points to another node after inserting" do
+      linked_list.insert_first("node 1")
+      linked_list.insert_first("node 2")
+      expect(linked_list.first.other).to be_a(Node)
+    end
+
+    it "points to another node after inserting" do
+      linked_list.insert_first("node 1")
+      linked_list.insert_first("node 2")
+      expect(linked_list.first.other.value).to eq("node 1")
+    end
+
+  end
+
 end
