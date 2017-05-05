11d8e9af8d8c1f7f7f16958ec296a1d727ef9bb1
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-05-04 19:28:23 -0700

    Initialize node and write rspec tests

--- node_spec.rb
+++ node_spec.rb
@@ -2,4 +2,18 @@ require_relative 'node'
 
 describe Node do
 
+  describe "let" do
+
+    let(:node) { Node.new("first") }
+
+    it "creates a new node with value 'first'" do
+      expect(node.value).to eq "first"
+    end
+
+    it "doesn't point to anything yet" do
+      expect(node.other).to be nil
+    end
+
+  end
+
 end
