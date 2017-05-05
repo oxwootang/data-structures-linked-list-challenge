73e6238ceb380ebcb99cbf71e7afc096d42c55b1
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-05-04 19:32:32 -0700

    Implement Node#insert_after and Node#remove_after and write rspec tests

--- node_spec.rb
+++ node_spec.rb
@@ -5,12 +5,25 @@ describe Node do
   describe "let" do
 
     let(:node) { Node.new("first") }
+    let(:node_2) { Node.new("second") }
 
     it "creates a new node with value 'first'" do
       expect(node.value).to eq "first"
     end
 
     it "doesn't point to anything yet" do
+      expect(node.other).to be nil
+    end
+
+    it "points to another node" do
+      node.insert_after(node_2)
+      expect(node.other).to eq(node_2)
+    end
+
+    it "removes the connection to another node" do
+      node_2.insert_after(node)
+      node.insert_after(node)
+      node.remove_after
       expect(node.other).to be nil
     end
 
